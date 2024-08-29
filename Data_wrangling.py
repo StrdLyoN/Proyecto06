d9dcd2f621e35f92639b529d2466eb9fb709dc6d
Alejandro Estrada <estrada_ln@hotmail.com>
2024-08-29 16:35:35 -0600

    Mantenible, Escalable y Confiable

--- Data_wrangling.py
+++ Data_wrangling.py
@@ -3,5 +3,16 @@ import pandas as pd
 def clean(datos):
     return datos.dropna() 
 
+class Limpiador():
+    
+    nombre = 'Sin nombre'
+    datos = None
+    def __init__(self, nombre_personalizado,datos_personzalizados):
+        self.nombre = nombre_personalizado
+        self.datos = datos_personzalizados
+
+    def clean(self):
+        return self.datos.dropna()
+
 if __name__ == '__main__':
     print('Es una libreria de limpieza de datos')
--- app.py
+++ app.py
@@ -3,11 +3,17 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-import Data_wrangling
+from Data_wrangling import Limpiador
 
 def main():
-    datos = pd.read_csv('./vehicles_us.csv')
-    datos = Data_wrangling.clean(datos)
+    datos_raw = pd.read_csv('./vehicles_us.csv')
+    limpiador = Limpiador('Limpiador',datos_raw)
+
+    print(datos_raw.isna().sum())
+    datos_clean = limpiador.clean()
+
+    print(datos_clean.isna().sum())
+
 
     st.header('Vehicles Graphics')
     hist_button = st.button('Make a Histogram')
@@ -15,12 +21,12 @@ def main():
 
     if hist_button:
         st.write('Histogram of vehicles_us')
-        fig = px.histogram(datos, x='odometer', y='price')
+        fig = px.histogram(datos_clean, x='odometer', y='price')
         st.plotly_chart(fig, use_container_width=True)
     
     if disp_button:
         st.write('Scatterplot of Vehicles_usd')
-        fig01 = px.scatter(datos, x='odometer', y='price')
+        fig01 = px.scatter(datos_clean, x='odometer', y='price')
         st.plotly_chart(fig01, use_container_width=True)
 
 if __name__ == '__main__':
